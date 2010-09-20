b499dc2aaa985af4f568e90136b01bc23945ae65
Adam Fast <adam@adamfast.com>
2010-09-20 21:14:05 +0000

    Add missing imports for GFK items.

--- socialregistration/managers.py
+++ socialregistration/managers.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
 class FacebookProfileManager(models.Manager):
