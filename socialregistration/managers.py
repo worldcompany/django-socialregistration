3daff1450e7795654cdafab4075ce857915750a0
Cody Soyland <cody@soyland.org>
2011-06-27 19:08:40 -0500

    Add site filtering to SocialProfileManager.

--- socialregistration/managers.py
+++ socialregistration/managers.py
@@ -1,17 +1,24 @@
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.sites.models import Site
 from django.db import models
 
 class SocialProfileManager(models.Manager):
     def for_user_by_username(self, username):
         user = User.objects.get(username=username)
-        return self.get(content_type=ContentType.objects.get_for_model(User), object_id=user.id)
+        return self.for_user_by_id(user.pk)
+
+    def on_current_site(self):
+        return self.filter(site=Site.objects.get_current())
 
     def for_user_by_id(self, user_id):
-        return self.get(content_type=ContentType.objects.get_for_model(User), object_id=user_id)
+        return self.on_current_site().get(
+            content_type=ContentType.objects.get_for_model(User),
+            object_id=user_id)
 
     def for_object_content_type(self, obj):
-        return self.filter(content_type=ContentType.objects.get_for_model(obj.__class__))
+        return self.on_current_site().filter(
+            content_type=ContentType.objects.get_for_model(obj.__class__))
 
     def for_object(self, obj):
-        return self.get(content_type=ContentType.objects.get_for_model(obj.__class__), object_id=obj.pk)
+        return self.for_object_content_type(obj).get(object_id=obj.pk)
